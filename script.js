ffbe412afa7d9ab838a302721d763aa459632a4b
Vanderlei <vanderleijr2001@gmail.com>
2023-12-02 12:50:32 -0800

    new button

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const itemForm = document.getElementById('item-form');
 const itemInput = document.getElementById('item-input');
 const itemList = document.getElementById('item-list');
+const clearBtn = document.getElementById('clear')
 
 function createButton (classes) {
    const button = document.createElement('button');
@@ -35,13 +36,20 @@ function addItem(e) {
     itemList.appendChild(li);
 
     itemInput.value = '';
-
-
 }
- 
+function removeItem (e) {
+   if (e.target.parentElement.classList.contains('remove-item')) {
+      e.target.parentElement.parentElement.remove();
+   } 
+      }
 
-
+function clearItem(e){
+   while (itemList.firstChild) {
+      itemList.removeChild(itemList.firstChild);
+   }
+}
+// Events Listeners ()
 itemForm.addEventListener('submit', addItem)
-
-
+itemList.addEventListener('click', removeItem)
+clearBtn.addEventListener('click', clearItem)
   
